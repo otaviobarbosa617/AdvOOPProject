b7452085c0300961464c1720e889752de7cc098e
otaviobarbosa617 <barbosaoapb@gmail.com>
2021-12-02 23:24:40 -0500

    using on top of CustomerManager

--- AdvOOPProject/CustomerManager.cs
+++ AdvOOPProject/CustomerManager.cs
@@ -1,4 +1,11 @@
-﻿namespace AdvOOPProject
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace AdvOOPProject
 {
     class CustomerManager
     {
